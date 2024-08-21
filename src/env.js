743f81b46f08c95a7391d3598febf2f217d17e52
Nathaniel Broughton <ndbroughton6@gmail.com>
2024-08-20 23:41:56 -0400

    add env vars

--- src/env.js
+++ src/env.js
@@ -1,6 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+const optionalBoolSchema = z
+  .enum(["true", "false"])
+  .optional()
+  .transform((val) => val === "true");
+
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
@@ -8,6 +13,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    DATA_ENCRYPTION_PASSPHRASE: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -20,10 +26,17 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
+    FILE_STORAGE_CDN_URL: z.string().optional(),
+    FILE_STORAGE_ENDPOINT: z.string(),
+    FILE_STORAGE_KEY: z.string(),
+    FILE_STORAGE_REGION: z.string(),
+    FILE_STORAGE_SECRET: z.string(),
+    FILE_STORAGE_BUCKET: z.string(),
+    FILE_STORAGE_PATH_STYLE: optionalBoolSchema,
   },
 
   /**
@@ -41,11 +54,19 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATA_ENCRYPTION_PASSPHRASE: process.env.DATA_ENCRYPTION_PASSPHRASE,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    FILE_STORAGE_CDN_URL: process.env.FILE_STORAGE_CDN_URL,
+    FILE_STORAGE_ENDPOINT: process.env.FILE_STORAGE_ENDPOINT,
+    FILE_STORAGE_KEY: process.env.FILE_STORAGE_KEY,
+    FILE_STORAGE_REGION: process.env.FILE_STORAGE_REGION,
+    FILE_STORAGE_SECRET: process.env.FILE_STORAGE_SECRET,
+    FILE_STORAGE_BUCKET: process.env.FILE_STORAGE_BUCKET,
+    FILE_STORAGE_PATH_STYLE: process.env.FILE_STORAGE_PATH_STYLE,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
