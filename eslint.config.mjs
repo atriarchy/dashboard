cbb0dccdb9dd2875641ee7a0f78d781d80322aaa
Dylan Hackworth <me@dylhack.dev>
2024-08-24 23:13:05 -0500

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,49 +8,61 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-), {
+    "plugin:@typescript-eslint/stylistic-type-checked"
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "script",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
 
-        parserOptions: {
-            project: true,
-        },
+      parserOptions: {
+        project: true,
+      },
     },
 
     rules: {
-        "@typescript-eslint/array-type": "off",
-        "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
 
-        "@typescript-eslint/consistent-type-imports": ["warn", {
-            prefer: "type-imports",
-            fixStyle: "inline-type-imports",
-        }],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
 
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
 
-        "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/require-await": "off",
 
-        "@typescript-eslint/no-misused-promises": ["error", {
-            checksVoidReturn: {
-                attributes: false,
-            },
-        }],
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
     },
-}];
+  },
+];
