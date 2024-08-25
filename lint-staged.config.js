f9e44837ecbb374a7586d20a9d4197b40d635140
ACA30 <me@nateb.ca>
2024-08-24 23:06:49 -0400

    Add lint step to commit hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,10 @@
 const config = {
-  "*": "prettier --write --ignore-unknown",
+  "*/**/*.{js,jsx,ts,tsx}": [
+    "prettier --write --ignore-unknown",
+    "SKIP_ENV_VALIDATION=1 eslint --fix",
+    "SKIP_ENV_VALIDATION=1 eslint",
+  ],
+  "*/**/*.{json,css,md}": ["prettier --write"],
 };
 
 export default config;
