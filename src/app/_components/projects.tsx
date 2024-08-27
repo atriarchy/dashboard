84cf4b4496623b50d2af024093619812201ae5dc
ACA30 <me@nateb.ca>
2024-08-27 15:05:18 -0400

    pretty project cards

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -2,9 +2,9 @@
 
 import Link from "next/link";
 import { Fragment } from "react";
-
 import { api } from "@/trpc/react";
 import { ProjectForm } from "@/app/_components/project-form";
+import Badge from "./primitives/badge";
 
 export function Projects({ access }: { access?: "ADMIN" | null }) {
   const projects = api.project.getProjects.useInfiniteQuery(
@@ -13,6 +13,14 @@ export function Projects({ access }: { access?: "ADMIN" | null }) {
       getNextPageParam: lastPage => lastPage?.cursor,
     }
   );
+
+  const statusColorMap = {
+    DRAFT: "gray",
+    ACTIVE: "green",
+    CLOSED: "red",
+    RELEASED: "purple",
+  };
+
   return (
     <>
       {projects.error ? (
@@ -25,16 +33,47 @@ export function Projects({ access }: { access?: "ADMIN" | null }) {
             </h1>
             {access === "ADMIN" && <ProjectForm />}
           </div>
-          <div className="grid w-full grid-cols-2 gap-2">
+          <div className="grid w-full grid-cols-3 gap-2">
             {projects.data?.pages.map((group, i) => (
               <Fragment key={i}>
                 {group?.data.map(project => (
                   <Link
                     href={`/dashboard/projects/${project.username}`}
                     key={project.username}
-                    className="w-full break-words rounded-lg bg-gray-800 px-4 py-2 transition-colors hover:bg-gray-800/80 disabled:bg-gray-800/50"
+                    className="flex w-full break-words rounded-lg bg-neutral-800 transition-colors hover:bg-neutral-800/80 disabled:bg-neutral-800/50"
                   >
-                    {project.title}
+                    {project.thumbnail && (
+                      <div className="flex-shrink-0">
+                        <img
+                          src={project.thumbnail}
+                          alt={project.title}
+                          className="h-16 w-16 rounded-l-lg object-cover"
+                        />
+                      </div>
+                    )}
+                    <div
+                      className={`flex flex-1 items-center justify-between truncate bg-neutral-800 px-4 py-2 ${
+                        project.thumbnail
+                          ? "rounded-r-lg border-l border-neutral-700"
+                          : "rounded-lg border border-neutral-700"
+                      }`}
+                    >
+                      <div className="flex-1 truncate text-sm">
+                        <span className="font-medium text-neutral-100">
+                          {project.title}
+                        </span>
+                        <span className="ml-2">
+                          <Badge
+                            text={project.status}
+                            color={statusColorMap[project.status]}
+                            dark
+                          />
+                        </span>
+                        <p className="text-gray-400">
+                          {`${project.trackCount} track${project.trackCount !== 1 ? "s" : ""}`}
+                        </p>
+                      </div>
+                    </div>
                   </Link>
                 ))}
               </Fragment>
