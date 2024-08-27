5b06bd93c851831eef3ddf1e6110963d31f1647c
ACA30 <me@nateb.ca>
2024-08-27 14:39:50 -0400

    fix badges

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,21 @@ export default {
       },
     },
   },
+  safelist: [
+    {
+      pattern:
+        /bg-(gray|red|yellow|green|blue|indigo|purple|pink)-(50|100|400|700|600\/10|400\/20)/,
+      variants: ["dark", "flat"],
+    },
+    {
+      pattern: /text-(gray|red|yellow|green|blue|indigo|purple|pink)-(400|700)/,
+      variants: ["dark", "flat"],
+    },
+    {
+      pattern:
+        /ring-(gray|red|yellow|green|blue|indigo|purple|pink)-(600\/10|400\/20)/,
+      variants: ["dark", "flat"],
+    },
+  ],
   plugins: [require("@tailwindcss/forms")],
 } satisfies Config;
