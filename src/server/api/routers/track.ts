fac341783222fc0106a4c73337293ececa7b3263
ACA30 <me@nateb.ca>
2024-08-28 04:33:54 -0400

    fix discord users oops

--- src/server/api/routers/track.ts
+++ src/server/api/routers/track.ts
@@ -70,10 +70,8 @@ export const trackRouter = createTRPCRouter({
             if (collaborator.discordUserId) {
               return {
                 type: "DISCORD",
-                discord: {
-                  username: collaborator.discordUsername,
-                  avatar: collaborator.discordAvatar,
-                },
+                username: collaborator.discordUsername,
+                avatar: collaborator.discordAvatar,
                 role: collaborator.role,
               };
             }
