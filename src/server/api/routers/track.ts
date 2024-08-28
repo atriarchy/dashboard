951e91224c6fe8dca28e8bbbc6084d40e38899f0
ACA30 <me@nateb.ca>
2024-08-28 01:39:38 -0400

    Fix support for Discord avatars on tracks page

--- src/server/api/routers/track.ts
+++ src/server/api/routers/track.ts
@@ -65,10 +65,8 @@ export const trackRouter = createTRPCRouter({
             if (collaborator.discordUserId) {
               return {
                 type: "DISCORD",
-                discord: {
-                  username: collaborator.discordUsername,
-                  avatar: collaborator.discordAvatar,
-                },
+                username: collaborator.discordUsername,
+                avatar: collaborator.discordAvatar,
               };
             }
 
